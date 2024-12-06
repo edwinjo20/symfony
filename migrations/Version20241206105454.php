89071596e87953dcaec923832691fd3298362d58
edwinjo15 <edwinjones.m@gm&il.com>
2024-12-06 12:52:41 +0100

    modified

---
+++ migrations/Version20241206105454.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241206105454 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(100) NOT NULL, ADD last_name VARCHAR(100) NOT NULL, ADD api_toke VARCHAR(255) DEFAULT NULL, DROP username, CHANGE roles roles JSON DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user ADD username VARCHAR(255) NOT NULL, DROP first_name, DROP last_name, DROP api_toke, CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,88 +9,98 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     #[Route('/users', name: 'get_users', methods: ['GET'])]
-    public function getAllUsers(): JsonResponse
+
+    public function index(UserRepository $userRepository): JsonResponse
     {
-        $user = $this->getUser();
-        dump($user->getRoles()); // Ensure the user has ROLE_USER
-        return $this->json(['message' => 'Access successful']);
+        $users = $userRepository->findAll();
+        return $this->json($users);
     }
-    
     #[Route('/users/{id}', name: 'get_user', methods: ['GET'])]
-    public function getUserById(User $user): JsonResponse
+
+    public function show(UserRepository $userRepository, $id): JsonResponse
     {
+        $user = $userRepository->find($id);
+        if (!$user) {
+            return $this->json(['message' => 'User not found'], 404);
+        }
         return $this->json($user);
     }
 
     #[Route('/users', name: 'create_user', methods: ['POST'])]
-    public function createUser(
-        Request $request,
-        UserPasswordHasherInterface $passwordHasher,
-        EntityManagerInterface $entityManager
-    ): JsonResponse {
+    public function create(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse{
+
+    
         $data = json_decode($request->getContent(), true);
-    
-        if (!$data['username'] || !$data['email'] || !$data['password']) {
-            return $this->json(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
-        }
-    
-        // Log the incoming data
-        file_put_contents('php://stderr', print_r($data, true));
-    
+
         $user = new User();
-        $user->setUsername($data['username']);
+        $user->setFirstName($data['firstName']);
+        $user->setLastName($data['lastName']);
         $user->setEmail($data['email']);
-        $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
-        $user->setRoles(['ROLE_USER']);
+        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+        
+        $user->setPassword($hashedPassword);        $user->setRoles($data['roles']);
     
         $entityManager->persist($user);
         $entityManager->flush();
-    
-        return $this->json($user, Response::HTTP_CREATED);
+
+        return new JsonResponse(['status'=>'User created'], JsonResponse::HTTP_CREATED);
     }
+
     #[Route('/users/{id}', name: 'update_user', methods: ['PUT'])]
-    public function updateUser(
-        Request $request,
-        User $user,
-        UserPasswordHasherInterface $passwordHasher,
-        EntityManagerInterface $entityManager
-    ): JsonResponse {
+    public function update(EntityManagerInterface $entityManager, Request $request, User $user , UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
         $data = json_decode($request->getContent(), true);
+    
+        if ($data) {
+            // Mettre à jour les champs de l'utilisateur
+            if (isset($data['firstName'])) {
+                $user->setFirstName($data['firstName']);
+            }
+            if (isset($data['lastName'])) {
+                $user->setLastName($data['lastName']);
+            }
+            if (isset($data['email'])) {
+                $user->setEmail($data['email']);
+            }
+            if (isset($data['password'])) {
+                // Hachage du mot de passe
+                $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+                $user->setPassword($hashedPassword);
+            }
+            if (isset($data['roles'])) {
+                $user->setRoles($data['roles']);
+            }
 
-        if (isset($data['username'])) {
-            $user->setUsername($data['username']);
-        }
-        if (isset($data['email'])) {
-            $user->setEmail($data['email']);
-        }
-        if (isset($data['password'])) {
-            $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+            // Sauvegarder les modifications
+            $entityManager->flush();
+
+            return $this->json($user);
         }
 
-        $entityManager->flush();
-
-        return $this->json($user);
+        return $this->json(['message' => 'Invalid data'], 400);
     }
+
 
     #[Route('/users/{id}', name: 'delete_user', methods: ['DELETE'])]
-    public function deleteUser(User $user, EntityManagerInterface $entityManager): JsonResponse
+    public function delete(EntityManagerInterface $entityManager, User $user): JsonResponse
     {
+        // Supprimer l'utilisateur
         $entityManager->remove($user);
         $entityManager->flush();
-
-        return $this->json(['message' => 'User deleted successfully'], Response::HTTP_NO_CONTENT);
+        
+        return $this->json(['status' => 'User deleted']);
     }
+
 
     #[Route('/login', name: 'login', methods: ['POST'])]
     public function login(): JsonResponse
     {
         throw new \Exception('The JWT authentication system should handle this request.');
     }
-    
 }
